7f408c98b307517e99347ffec75cd99475e1a789
Ali <p156121@nu.edu.pk.com>
2019-10-02 08:33:58 +0500

    Header Completed

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React ,{Component} from 'react';
+
+export default function Header({children,title,styleClass}){
+  return(
+    <header>
+     <div className='container-fluid'>
+      <div className={`row align-items-center ${styleClass}` }>
+       <div className='col text-center'>
+        <h1 className='text-light text-uppercase display-3 text-slanted letter-spacing'> {title} </h1>
+        {children}
+       </div>
+      </div>
+     </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  title:'Default Title',
+  styleClass:'header-hero'
+};
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import Header from '../Components/Header';
+import {Link} from 'react-router-dom';
 
 export default class Home extends Component{
   render(){
     return(
-      <h4>Hello from HOME</h4>
+      <div>
+        <Header title='amazing recipes'>
+          <Link to='recipes' className='text-uppercase btn btn-secondary btn-lg mt-3'> search recipes </Link>
+        </Header>
+      </div>
     );
   }
 }
